59feeeaab0d2d1e53570613c5bff3d53a1a8b3fd
gentoo <82971071+sprmcell@users.noreply.github.com>
2022-11-17 12:31:25 +0200

    not much

--- dwm/config.h
+++ dwm/config.h
@@ -68,8 +68,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 /* UNCOMMENT DMENUCMD IN KEYS IF YOU USE IT*/
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 
-/* REPLACE ALACRITTY WITH YOUR TERMINAL */
-static const char *termcmd[]  = { "alacritty", NULL };
+/* REPLACE ST WITH YOUR TERMINAL */
+static const char *termcmd[]  = { "st", NULL };
 
 /* ROFI */
 static const char *rofi[]     = { "rofi", "-show", "drun", "-show-icons", NULL };
