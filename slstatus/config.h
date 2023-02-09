d5e24cbe590a96e7323b95ce56580ad0de2c96fe
install gentoo <82971071+sprmcell@users.noreply.github.com>
2023-02-09 15:48:43 +0200

    doing some time configs

--- slstatus/config.h
+++ slstatus/config.h
@@ -63,10 +63,10 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	//{ datetime, "%s",           "%F %T" },
+	{ datetime, "%s",           "%F %T" },
 	//{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
     //{ netspeed_rx, "%sB/s  ", "enp0s3" },
-	{ datetime, "%s",           "%a %b %d " },
+	//{ datetime, "%s",           "%a %b %d " },
 	{ cpu_perc, "  %s%% |   ", NULL	      },
 	{ ram_perc, "  %s%% |   ", NULL	      },
 };
